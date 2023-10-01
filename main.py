2ba935b2e69673b1239fe7a3905aacd1b98ded78
Pedro Germano <pedro.cruz@cear.ufpb.br>
2023-10-01 20:25:34 -0300

    Corrige main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import card as crd 
 
 card = crd.ATPCard() 
-dc = crd.DCSource()
-dc.line = "11XX0001 0      1.E4                                               -1.      1.E3"
-card.source.add_source(dc)
+
 card.write_to_file("test.txt")
