975ab89406adc05157933dc5b700607fb44ec82b
Hyago Fellipe <hyago.soares@cear.ufpb.br>
2023-12-27 11:01:14 -0300

    branch.py refactor

--- src/branch.py
+++ src/branch.py
@@ -1,5 +1,5 @@
 import copy 
-from utils import numeric_to_valid_str, is_str_true
+from utils import numeric_to_valid_str, is_str_true, cant_be_longer_message
 
 class Branch:
     def __init__(self):
@@ -101,11 +101,14 @@ class RlcElementBasic(RlcElement):
     
     @itype.setter
     def itype(self, new_itype):
-        if len(new_itype) > 2:
-            raise ValueError("Itype cannot be longer than 2 characters")
+
+        spot_len = 2
+
+        if len(new_itype) > spot_len:
+            raise ValueError(cant_be_longer_message('new_itype', spot_len))
         
-        self._line = new_itype.ljust(2) + self._line[2:]
-        self._itype = new_itype.ljust(2)
+        self._line = new_itype.ljust(spot_len) + self._line[spot_len:]
+        self._itype = new_itype.ljust(spot_len)
 
     @property
     def node_1(self):
@@ -113,51 +116,71 @@ class RlcElementBasic(RlcElement):
     
     @node_1.setter
     def node_1(self, new_node_1):
-        if len(new_node_1) > 6:
-            raise ValueError("Node 1 cannot be longer than 6 characters")
         
-        self._line = new_node_1.ljust(6) + self._line[6:]
+        spot_len = 6
+        init_index = 2
+        end_index = 8
+        
+        if len(new_node_1) > spot_len:
+            raise ValueError(cant_be_longer_message('new_node_1', spot_len))
+        
+        self._line = self._line[0:init_index] + new_node_1.ljust(spot_len) + self._line[end_index:]
 
-        self._node_1 = new_node_1.ljust(6)
+        self._node_1 = new_node_1.ljust(spot_len)
     
     @property
     def node_2(self):
         return self._node_2
     
-    @node_2.setter
+    @node_2.setter     
     def node_2(self, new_node_2):
-        if len(new_node_2) > 6:
-            raise ValueError("Node 2 cannot be longer than 6 characters")
         
-        self._line = self._line[0:8] + new_node_2.ljust(6) + self._line[14:]
+        spot_len = 6
+        init_index = 8
+        end_index = 14
+        
+        if len(new_node_2) > spot_len:
+            raise ValueError(cant_be_longer_message('new_node_2', spot_len))
+        
+        self._line = self._line[0:init_index] + new_node_2.ljust(spot_len) + self._line[end_index:]
 
-        self._node_2 = new_node_2.ljust(6)
+        self._node_2 = new_node_2.ljust(spot_len)
     
     @property
     def node_ref_1(self):
         return self._node_ref_1
     
-    @node_ref_1.setter
+    @node_ref_1.setter     
     def node_ref_1(self, new_node_ref_1):
-        if len(new_node_ref_1) > 6:
-            raise ValueError("Node ref 1 cannot be longer than 6 characters")
         
-        self._line = self._line[0:14] + new_node_ref_1.ljust(6) + self._line[20:]
+        spot_len = 6
+        init_index = 14
+        end_index = 20
+        
+        if len(new_node_ref_1) > spot_len:
+            raise ValueError(cant_be_longer_message('new_node_ref_1', spot_len))
+        
+        self._line = self._line[0:init_index] + new_node_ref_1.ljust(spot_len) + self._line[end_index:]
 
-        self._node_ref_1 = new_node_ref_1.ljust(6)
+        self._node_ref_1 = new_node_ref_1.ljust(spot_len)
 
     @property
     def node_ref_2(self):
         return self._node_ref_2
     
-    @node_ref_2.setter
+    @node_ref_2.setter     
     def node_ref_2(self, new_node_ref_2):
-        if len(new_node_ref_2) > 6:
-            raise ValueError("Node ref 2 cannot be longer than 6 characters")
         
-        self._line = self._line[0:20] + new_node_ref_2.ljust(6) + self._line[26:]
+        spot_len = 6
+        init_index = 20
+        end_index = 26
+        
+        if len(new_node_ref_2) > spot_len:
+            raise ValueError(cant_be_longer_message('new_node_ref_2', spot_len))
+        
+        self._line = self._line[0:init_index] + new_node_ref_2.ljust(spot_len) + self._line[end_index:]
 
-        self._node_ref_2 = new_node_ref_2.ljust(6)
+        self._node_ref_2 = new_node_ref_2.ljust(spot_len)
     
     @property
     def resistance(self):
@@ -173,15 +196,17 @@ class RlcElementBasic(RlcElement):
                 raise ValueError("Resistance must be a numeric value")
 
         new_resistance = numeric_to_valid_str(new_resistance)
-
-
-
-        if len(new_resistance) > 6:
-            raise ValueError("Resistance cannot be longer than 6 characters")
         
-        self._line = self._line[0:26] + new_resistance.ljust(6) + self._line[32:]
+        spot_len = 6
+        init_index = 26
+        end_index = 32
+        
+        if len(new_resistance) > spot_len:
+            raise ValueError(cant_be_longer_message('new_resistance', spot_len))
+        
+        self._line = self._line[0:init_index] + new_resistance.ljust(spot_len) + self._line[end_index:]
 
-        self._resistance = new_resistance.ljust(6)
+        self._resistance = new_resistance.ljust(spot_len)
     
     @property
     def inductance(self):
@@ -189,8 +214,12 @@ class RlcElementBasic(RlcElement):
     
     @inductance.setter
     def inductance(self, new_inductance):
-
-        if new_inductance != " " * 6:
+             
+        spot_len = 6
+        init_index = 32
+        end_index = 38
+        
+        if new_inductance != " ".ljust(spot_len):
             try:
                 float(new_inductance)
             except ValueError:
@@ -198,15 +227,15 @@ class RlcElementBasic(RlcElement):
         
         new_inductance = numeric_to_valid_str(
             new_inductance, 
-            is_inductance=(not is_str_true(self.miscellaneous.x_opt))
-        )
+            is_inductance=(not is_str_true(self.miscellaneous.x_opt)) # if x_opt is true, then inductance unit is ohm, as in impedance
+        ) 
 
-        if len(new_inductance) > 6:
-            raise ValueError("Inductance cannot be longer than 6 characters")
+        if len(new_inductance) > spot_len:
+            raise ValueError(cant_be_longer_message('new_inductance', 6))
         
-        self._line = self._line[0:32] + new_inductance.ljust(6) + self._line[38:]
+        self._line = self._line[0:init_index] + new_inductance.ljust(spot_len) + self._line[end_index:]
 
-        self._inductance = new_inductance.ljust(6)
+        self._inductance = new_inductance.ljust(spot_len)
     
     @property
     def capacitance(self):
@@ -215,20 +244,24 @@ class RlcElementBasic(RlcElement):
     @capacitance.setter
     def capacitance(self, new_capacitance):
 
-        if new_capacitance != " " * 6:
+        spot_len = 6
+        init_index = 38
+        end_index = 44
+
+        if new_capacitance != " ".ljust(spot_len):
             try:
                 float(new_capacitance)
             except ValueError:
                 raise ValueError("Capacitance must be a numeric value")
             
         new_capacitance = numeric_to_valid_str(new_capacitance, is_capacitance=True)
-
-        if len(new_capacitance) > 6:
-            raise ValueError("Capacitance cannot be longer than 6 characters")
         
-        self._line = self._line[0:38] + new_capacitance.ljust(6) + self._line[44:]
+        if len(new_capacitance) > spot_len:
+            raise ValueError(cant_be_longer_message('new_capacitance', 6))
         
-        self._capacitance = new_capacitance.ljust(6)
+        self._line = self._line[0:init_index] + new_capacitance.ljust(spot_len) + self._line[end_index:]
+        
+        self._capacitance = new_capacitance.ljust(spot_len)
     
     @property
     def iout(self):
@@ -236,12 +269,16 @@ class RlcElementBasic(RlcElement):
     
     @iout.setter
     def iout(self, new_iout):
-        if len(new_iout) > 1:
-            raise ValueError("Iout cannot be longer than 1 character")
-        
-        self._line = self._line[0:79] + new_iout
 
-        self._iout = new_iout.ljust(1)
+        spot_len = 1
+        init_index = 79
+
+        if len(new_iout) > spot_len:
+            raise ValueError(cant_be_longer_message("new_iout", spot_len))
+        
+        self._line = self._line[0:init_index] + new_iout
+
+        self._iout = new_iout.ljust(spot_len)
 
     @property
     def line(self):
@@ -249,10 +286,11 @@ class RlcElementBasic(RlcElement):
     
     @line.setter
     def line(self, new_line):
-        if len(new_line) > 80:
-            raise ValueError("Line can't be longer than be 80 characters long")
+
+        if len(new_line) > self.line_len:
+            raise ValueError(cant_be_longer_message("new_line", self.line_len))
         
-        self._line = new_line.ljust(80)
+        self._line = new_line.ljust(self.line_len)
 
         self._itype = new_line[0:2]
         self._node_1 = new_line[2:8]
@@ -308,115 +346,159 @@ class RlcElementDistParams(RlcElement):
     
     @itype.setter
     def itype(self, new_itype):
-        if len(new_itype) > 2:
-            raise ValueError("Itype cannot be longer than 2 characters long")
-        
-        self._line = new_itype.ljust(2) + self._line[2:]
 
-        self._itype = new_itype.ljust(2)
+        spot_len = 2
+
+
+        if len(new_itype) > spot_len:
+            raise ValueError(cant_be_longer_message("new_itype", spot_len))
+        
+        self._line = new_itype.ljust(spot_len) + self._line[2:]
+
+        self._itype = new_itype.ljust(spot_len)
 
     @property
     def node_1(self):
         return self._node_1
     
-    @node_1.setter
+    @node_1.setter     
     def node_1(self, new_node_1):
-        if len(new_node_1) > 6:
-            raise ValueError("Node cannot be longer than be 6 characters long")
         
-        self._line = self._line[0:2] + new_node_1.ljust(6) + self._line[8:]
+        spot_len = 6
+        init_index = 2
+        end_index = 8
+        
+        if len(new_node_1) > spot_len:
+            raise ValueError(cant_be_longer_message(new_node_1, spot_len))
+        
+        self._line = self._line[0:init_index] + new_node_1.ljust(spot_len) + self._line[end_index:]
 
-        self._node_1 = new_node_1.ljust(6)
+        self._node_1 = new_node_1.ljust(spot_len)
     
     @property
     def node_2(self):
         return self._node_2
     
-    @node_2.setter
+    @node_2.setter     
     def node_2(self, new_node_2):
-        if len(new_node_2) > 6:
-            raise ValueError("Node cannot be longer than be 6 characters long")
         
-        self._line = self._line[0:8] + new_node_2.ljust(6) + self._line[14:]
+        spot_len = 6
+        init_index = 8
+        end_index = 14
+        
+        if len(new_node_2) > spot_len:
+            raise ValueError(cant_be_longer_message(new_node_2, spot_len))
+        
+        self._line = self._line[0:init_index] + new_node_2.ljust(spot_len) + self._line[end_index:]
 
-        self._node_2 = new_node_2.ljust(6)
+        self._node_2 = new_node_2.ljust(spot_len)
 
     @property
     def node_ref_1(self):
         return self._node_ref_1
     
-    @node_ref_1.setter
+    @node_ref_1.setter     
     def node_ref_1(self, new_node_ref_1):
-        if len(new_node_ref_1) > 6:
-            raise ValueError("Node cannot be longer than be 6 characters long")
         
-        self._line = self._line[0:14] + new_node_ref_1.ljust(6) + self._line[20:]
+        spot_len = 6
+        init_index = 14
+        end_index = 20
+        
+        if len(new_node_ref_1) > spot_len:
+            raise ValueError(cant_be_longer_message(new_node_ref_1, spot_len))
+        
+        self._line = self._line[0:init_index] + new_node_ref_1.ljust(spot_len) + self._line[end_index:]
 
-        self._node_ref_1 = new_node_ref_1.ljust(6)
+        self._node_ref_1 = new_node_ref_1.ljust(spot_len)
 
     @property
     def node_ref_2(self):
         return self._node_ref_2
     
-    @node_ref_2.setter
+    @node_ref_2.setter     
     def node_ref_2(self, new_node_ref_2):
-        if len(new_node_ref_2) > 6:
-            raise ValueError("Node cannot be longer than be 6 characters long")
         
-        self._line = self._line[0:20] + new_node_ref_2.ljust(6) + self._line[26:]
+        spot_len = 6
+        init_index = 20
+        end_index = 26
+        
+        if len(new_node_ref_2) > spot_len:
+            raise ValueError(cant_be_longer_message(new_node_ref_2, spot_len))
+        
+        self._line = self._line[0:init_index] + new_node_ref_2.ljust(spot_len) + self._line[end_index:]
 
-        self._node_ref_2 = new_node_ref_2.ljust(6)
+        self._node_ref_2 = new_node_ref_2.ljust(spot_len)
     
     @property
     def resistance(self):
         return self._resistance
     
-    @resistance.setter
+    @resistance.setter     
     def resistance(self, new_resistance):
-        if len(new_resistance) > 6:
-            raise ValueError("Resistance cannot be longer than 6 characters long")
         
-        self._line = self._line[0:26] + new_resistance.ljust(6) + self._line[32:]
+        spot_len = 6
+        init_index = 26
+        end_index = 32
+        
+        if len(new_resistance) > spot_len:
+            raise ValueError(cant_be_longer_message(new_resistance, spot_len))
+        
+        self._line = self._line[0:init_index] + new_resistance.ljust(spot_len) + self._line[end_index:]
 
-        self._resistance = new_resistance.ljust(6)
+        self._resistance = new_resistance.ljust(spot_len)
     
     @property
     def distribution_param_a(self):
         return self._distribution_param_a
     
-    @distribution_param_a.setter
+    @distribution_param_a.setter       
     def distribution_param_a(self, new_distribution_param_a):
-        if len(new_distribution_param_a) > 6:
-            raise ValueError("Distribution cannot be longer than be 6 characters long")
         
-        self._line = self._line[0:32] + new_distribution_param_a.ljust(6) + self._line[38:]
-        self._distribution_param_a = new_distribution_param_a.ljust(6)
+        spot_len = 6
+        init_index = 32
+        end_index = 38
+
+        if len(new_distribution_param_a) > spot_len:
+            raise ValueError(cant_be_longer_message(new_distribution_param_a, spot_len))
+        
+        self._line = self._line[0:init_index] + new_distribution_param_a.ljust(spot_len) + self._line[end_index:]
+        self._distribution_param_a = new_distribution_param_a.ljust(spot_len)
     
     @property
     def distribution_param_b(self):
         return self._distribution_param_b
     
-    @distribution_param_b.setter
+    @distribution_param_b.setter       
     def distribution_param_b(self, new_distribution_param_b):
-        if len(new_distribution_param_b) > 6:
-            raise ValueError("Distribution cannot be longer than be 6 characters long")
         
-        self._line = self._line[0:38] + new_distribution_param_b.ljust(6) + self._line[44:]
+        spot_len = 6
+        init_index = 38
+        end_index = 44
+        
+        if len(new_distribution_param_b) > spot_len:
+            raise ValueError(cant_be_longer_message(new_distribution_param_b, spot_len))
+        
+        self._line = self._line[0:init_index] + new_distribution_param_b.ljust(spot_len) + self._line[end_index:]
 
-        self._distribution_param_b = new_distribution_param_b.ljust(6)
+        self._distribution_param_b = new_distribution_param_b.ljust(spot_len)
     
     @property
     def length(self):
         return self._length
     
-    @length.setter
+    @length.setter     
     def length(self, new_length):
-        if len(new_length) > 6:
-            raise ValueError("Length cannot be longer than 6 characters long")
         
-        self._line = self._line[0:44] + new_length.ljust(6) + self._line[50:]
+        spot_len = 6
+        init_index = 44
+        end_index = 50
+        
+        if len(new_length) > spot_len:
+            raise ValueError(cant_be_longer_message(new_length, spot_len))
+        
+        self._line = self._line[0:init_index] + new_length.ljust(spot_len) + self._line[end_index:]
 
-        self._length = new_length.ljust(6)
+        self._length = new_length.ljust(spot_len)
 
     @property
     def iline(self):
@@ -424,11 +506,16 @@ class RlcElementDistParams(RlcElement):
 
     @iline.setter
     def iline(self, new_iline):
-        if len(new_iline) > 2:
-            raise ValueError("Iline cannot be longer than 2 characters long")
+
+        spot_len = 2
+        init_index = 50
+        end_index = 52
+
+        if len(new_iline) > spot_len:
+            raise ValueError(cant_be_longer_message("new_iline", spot_len))
         
-        self._line = self._line[0:50] + new_iline.ljust(2) + self._line[52:]
-        self._iline = new_iline.ljust(2)
+        self._line = self._line[0:init_index] + new_iline.ljust(spot_len) + self._line[end_index:]
+        self._iline = new_iline.ljust(spot_len)
 
     @property
     def ipunch(self):
@@ -436,11 +523,16 @@ class RlcElementDistParams(RlcElement):
     
     @ipunch.setter
     def ipunch(self, new_ipunch):
-        if len(new_ipunch) > 2:
-            raise ValueError("Ipunch cannot be longer than 2 characters long")
+    
+        spot_len = 2
+        init_index = 52
+        end_index = 54
+
+        if len(new_ipunch) > spot_len:
+            raise ValueError(cant_be_longer_message(new_ipunch, spot_len))
         
-        self._line = self._line[0:52] + new_ipunch.ljust(2) + self._line[54:]
-        self._ipunch = new_ipunch.ljust(2)
+        self._line = self._line[0:init_index] + new_ipunch.ljust(spot_len) + self._line[end_index:]
+        self._ipunch = new_ipunch.ljust(spot_len)
     
     @property
     def ipose(self):
@@ -448,20 +540,28 @@ class RlcElementDistParams(RlcElement):
     
     @ipose.setter
     def ipose(self, new_ipose):
-        if len(new_ipose) > 2:
-            raise ValueError("Ipose cannot be longer than 2 characters long")
+
+        spot_len = 2    
+        init_index = 54
+        end_index = 56
+
+        if len(new_ipose) > spot_len:
+            raise ValueError(cant_be_longer_message("new_ipose", spot_len))
         
-        self._line = self._line[0:54] + new_ipose.ljust(2) + self._line[56:]
-        self._ipose = new_ipose.ljust(2)
+        self._line = self._line[0:init_index] + new_ipose.ljust(spot_len) + self._line[end_index:]
+        self._ipose = new_ipose.ljust(spot_len)
     
     @property
     def iout(self):
         return self._iout
-    
     @iout.setter
     def iout(self, new_iout):
+
+        spot_len = 1 
+        init_index = 79
+
         if len(new_iout) > 1:
-            raise ValueError("Iout cannot be longer than 1 characters long")
+            raise ValueError(cant_be_longer_message("new_iout", spot_len)) 
         
-        self._line = self._line[0:79] + new_iout
-        self._iout = new_iout.ljust(1)
+        self._line = self._line[0:init_index] + new_iout
+        self._iout = new_iout.ljust(spot_len)
